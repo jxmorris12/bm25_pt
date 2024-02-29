d0cab9888acfc9f225f67bcbec082271e2077b34
Jack Morris <jxmorris12@gmail.com>
2024-02-29 11:37:32 -0500

    v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="bm25_pt",
-    version="0.0.2",
+    version="0.0.3",
     description="bm25 search algorithm in pytorch",
     author="Jack Morris",
     author_email="jxm3@cornell.edu",
